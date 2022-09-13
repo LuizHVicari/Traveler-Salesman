f1324aa66cf84062f1c536703cb1291aa29ba772
LuizHVicari <luizhenrique.vicari@gmail.com>
2022-09-13 14:47:18 -0300

    create md file output

--- greedy.py
+++ greedy.py
@@ -1,88 +1,6 @@
 from time import process_time
-from utils import file_reader, write_file
+from utils import file_reader, write_file, greedy_traveler_salesman, write_markdown
 
-
-def greedy_traveler_salesman(graph, first : int=0):
-    '''
-    receives a graph and returns the distance found for the traveler salesman, the path and the execution time
-    '''
-    traveled = [first]
-    distance_traveled = 0
-    current = first
-
-    start = process_time()
-
-    while(len(traveled) != len(graph)):
-        # resets the auxiliar variables for the next iteration
-        minimum_distance = False
-        minimum_distance_index = False
-
-        for distance_index in range(len(graph)):
-
-            # if the index is still available for travelling
-            if not(distance_index in traveled):
-
-                # sets the minimum distance as the first available sitance in the current position
-                if not(minimum_distance) and graph[current][distance_index] != 0 and distance_index != current:
-                    minimum_distance = graph[current][distance_index]
-                    minimum_distance_index = distance_index
-
-                # if a smaller distance is found, sets it as the new minimum distance
-                elif minimum_distance > graph[current][distance_index] and graph[current][distance_index] != 0:
-                    minimum_distance = graph[current][distance_index]
-                    minimum_distance_index = distance_index
-
-        # travels to the closest point and removes it from the available points for traveling
-        current = minimum_distance_index
-        traveled.append(current)
-        distance_traveled += minimum_distance
-
-    # if it is possible to travel from the last element to the first one  
-    if graph[current][first] != 0:
-        traveled.append(first)
-        traveled.remove(first)
-
-        distance_traveled += graph[minimum_distance_index][first]
-    
-    else:
-        return False
-
-        '''# list for the way back to the first element
-        back_traveling = [current]
-
-        # while it hasen't reached the first element
-        while(traveled[-1] != first):
-            minimum_distance = False
-            minimum_distance_index = False
-
-            # if it's possible to travel from the new last element to the first one
-            if graph[current][first] != 0:
-                traveled.append(current)
-                distance_traveled += graph[current][first]
-
-            # if it's not possible to do so, it will travel always to the closest element and try to reach the first element from there
-            else:
-                for distance_index in range(len(graph)):
-
-                    if not(distance_index in back_traveling):
-
-                        if not(minimum_distance) and graph[current][distance_index] != 0:
-                            minimum_distance = graph[current][distance_index]
-                            minimum_distance_index = distance_index
-
-                        elif minimum_distance > graph[current][distance_index] and graph[current][distance_index] != 0:
-                            minimum_distance = graph[current][distance_index]
-                            minimum_distance_index = distance_index
-
-        updates all variables for the TSP
-        current = minimum_distance_index
-        back_traveling.append(current)
-        distance_traveled += minimum_distance
-        traveled.append(current)
-        '''
-
-    end = process_time()
-    return distance_traveled, traveled, end - start
 
 if __name__ == '__main__':
     start_time = process_time()
@@ -90,17 +8,24 @@ if __name__ == '__main__':
     # list with all the atsp files on http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/atsp/
     files = ('TSPLIB/br17.atsp', 'TSPLIB/ft53.atsp', 'TSPLIB/ft70.atsp', 'TSPLIB/ftv33.atsp', 'TSPLIB/ftv35.atsp', 'TSPLIB/ftv38.atsp', 'TSPLIB/ftv44.atsp', 'TSPLIB/ftv47.atsp', 'TSPLIB/ftv55.atsp', 'TSPLIB/ftv64.atsp', 'TSPLIB/ftv70.atsp', 'TSPLIB/ftv170.atsp', 'TSPLIB/kro124p.atsp', 'TSPLIB/p43.atsp', 'TSPLIB/rbg323.atsp', 'TSPLIB/rbg358.atsp', 'TSPLIB/rbg403.atsp', 'TSPLIB/rbg443.atsp', 'TSPLIB/ry48p.atsp')
     output = ''
+    solution = list()
+    solution_time = list()
 
     # runs the greedy method on all the files listed above and appends its returned data on a string
     for file in files:
         data = greedy_traveler_salesman(file_reader(file)[0])
-        output += file.replace('TSPLIB/', '').replace('.atsp', '') + ':'
-        for i in range(8 - len(file.replace('TSPLIB/', '').replace('.atsp', ''))):
-            output += ' '
         
         if data != False:
-            output +=  f' distance: {data[0]}, execution time: {data[2]:.60f} s\n'
+            solution.append(f'{data[0]}')
+            solution_time.append(f'{data[2] * 1000}')
         else:
             output += 'Não há solução gulosa para este grafo\n'
+            solution.append('Não encontrada')
+            solution_time.append('Não se aplica')
 
-    write_file('Greedy output:\n' + output +f'total time: {process_time() - start_time} s', 'greedy_output.txt', 'w')
+    # formats the file name
+    graph_names = list()
+    for file in files:
+        graph_names.append(file.replace('TSPLIB/', '').replace('.atsp', ''))
+    best_solutions = ['39', '6905', '38673', '1286', '1473', '1530', '1613', '1776', '1608', '1839', '1950', '2755', '36230', '5620', '1326', '1163', '2465', '2720', '14422']
+    write_markdown(graph_name=graph_names, best_solutions=best_solutions, graph_solutions=solution, graph_time=solution_time, strategy='Estratégia Gulosa', name='GreedyOutput', title='Estudo estratégia gulosa para solução do problema do caixeiro viajante'.title())
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from cmath import inf
 from queue import PriorityQueue
-from typing import final
-
+from time import process_time
+import mdutils
 
 def file_reader(path : str):
     '''
@@ -27,9 +27,11 @@ def file_reader(path : str):
         
     return graph, dimension
 
+
 def write_file(text: str, path : str='default_output_file.txt', mode : str='w'):
     with open(path, mode) as output_file:
         output_file.write(text)
+
 
 def djikstra(graph : tuple, destiny : int=0, origin : int=0):
     queue_ = PriorityQueue()
@@ -57,6 +59,72 @@ def djikstra(graph : tuple, destiny : int=0, origin : int=0):
 
     return final_distance
 
+
+def greedy_traveler_salesman(graph, first : int=0):
+    '''
+    receives a graph and returns the distance found for the traveler salesman, the path and the execution time
+    '''
+    traveled = [first]
+    distance_traveled = 0
+    current = first
+
+    start = process_time()
+
+    while(len(traveled) != len(graph)):
+        # resets the auxiliar variables for the next iteration
+        minimum_distance = False
+        minimum_distance_index = False
+
+        for distance_index in range(len(graph)):
+
+            # if the index is still available for travelling
+            if not(distance_index in traveled):
+
+                # sets the minimum distance as the first available sitance in the current position
+                if not(minimum_distance) and graph[current][distance_index] != 0 and distance_index != current:
+                    minimum_distance = graph[current][distance_index]
+                    minimum_distance_index = distance_index
+
+                # if a smaller distance is found, sets it as the new minimum distance
+                elif minimum_distance > graph[current][distance_index] and graph[current][distance_index] != 0:
+                    minimum_distance = graph[current][distance_index]
+                    minimum_distance_index = distance_index
+
+        # travels to the closest point and removes it from the available points for traveling
+        current = minimum_distance_index
+        traveled.append(current)
+        distance_traveled += minimum_distance
+
+    # if it is possible to travel from the last element to the first one  
+    if graph[current][first] != 0:
+        traveled.append(first)
+        traveled.remove(first)
+
+        distance_traveled += graph[minimum_distance_index][first]
+    
+    else:
+        return False
+
+    end = process_time()
+    return distance_traveled, traveled, end - start
+
+
+def write_markdown(graph_name : list, best_solutions : list, graph_solutions : list, graph_time : list, strategy : str, name : str = 'Output', title : str = 'Title', author : str = 'Luiz Vicari',):
+
+    file = mdutils.MdUtils(file_name=name, title=title)
+    table_content = ['Grafo', 'Melhor solução conhecida', 'Solução encontrada', 'Tempo de Execução (ms)']
+
+    for g_name, g_time, g_best_solution, g_solution in zip(graph_name, graph_time, best_solutions, graph_solutions):
+        table_content.append(g_name)
+        table_content.append(g_best_solution)
+        table_content.append(g_solution)
+        table_content.append(g_time)
+
+    file.new_table(4, len(graph_name) + 1, text = table_content, text_align='center')
+
+    file.create_md_file()
+
+
 if __name__ == '__main__':
 
     graph = ((999, 3, 5, 6),
@@ -64,3 +132,4 @@ if __name__ == '__main__':
             (5, 7, 999, 8),
             (4, 1, 8, 999))
     print(djikstra(graph, 4, 0))
+    write_markdown('teste', 'teste')
